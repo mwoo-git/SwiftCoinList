5082d4a8c5d81f74f79753d040e1322961b409f3
WooMin <hanulbom@gmail.com>
2022-08-30 23:05:14 +0900

    Initial Commit

---
+++ SwiftCoinList/SwiftCoinListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinListApp.swift
+//  SwiftCoinList
+//
+//  Created by Woo Min on 2022/08/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
